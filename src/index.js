83a26a7e266e796cc983ef5a803dbdbd823ab7ca
Doug Szeto <douglas.szeto@mail.mcgill.ca>
2020-12-20 13:00:32 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Square extends React.Component {
+    render() {
+        return (
+            <input className="square" maxLength="1"></input>
+        )
+    }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Board extends React.Component {
+    renderSquare(i) {
+        return <Square value={i}/>;
+    }
+
+    renderRow(rowNum) {
+        let currentVal = rowNum * 9;
+        let row = Array(9);
+        for(let i=0; i<9; i++) {
+            
+            row[i] = <td>{this.renderSquare(currentVal)}</td>
+            currentVal++;
+        }
+
+        return(<tr>{row}</tr>);
+
+    }
+    render() {
+        return (
+            <div>
+                <div className="status">Status</div>
+                <br />
+
+                <table>
+                    {this.renderRow(0)}
+                    {this.renderRow(1)}
+                    {this.renderRow(2)}
+                    {this.renderRow(3)}
+                    {this.renderRow(4)}
+                    {this.renderRow(5)}
+                    {this.renderRow(6)}
+                    {this.renderRow(7)}
+                    {this.renderRow(8)}
+
+                </table>
+                {/* <div className="row">{this.renderRow(0)}</div>
+                <div className="row">{this.renderRow(1)}</div>
+                <div className="row">{this.renderRow(2)}</div>
+                <div className="row">{this.renderRow(3)}</div>
+                <div className="row">{this.renderRow(4)}</div>
+                <div className="row">{this.renderRow(5)}</div>
+                <div className="row">{this.renderRow(6)}</div>
+                <div className="row">{this.renderRow(7)}</div>
+                <div className="row">{this.renderRow(8)}</div> */}
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<Board />, document.getElementById('root'));
